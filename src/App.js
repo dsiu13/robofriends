b9fdf172a6d31534e5b417581f965e84e1229871
IamZombiez <dereksiu216@gmail.com>
2018-06-28 18:21:12 -0700

    starter update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Card from './components/card'
+import { robots } from './robots.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Card id={robots[0].id} name={robots[0].name}/>
+        <Card id={robots[1].id} name={robots[1].name}/>
+        <Card id={robots[2].id} name={robots[2].name}/>
       </div>
     );
   }

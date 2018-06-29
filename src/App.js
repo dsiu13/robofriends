95ba6e5b576f69070a0ab211979a7df4e9ad69d4
IamZombiez <dereksiu216@gmail.com>
2018-06-29 15:47:30 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Card from './components/card'
 import { robots } from './robots.js'
+import CardList from './components/cardList';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Card id={robots[0].id} name={robots[0].name}/>
-        <Card id={robots[1].id} name={robots[1].name}/>
-        <Card id={robots[2].id} name={robots[2].name}/>
+        <CardList />
       </div>
     );
   }
--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,8 @@
-
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+    <div>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>

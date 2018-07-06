6fec966e798adf36cf9e9e110ec2d298c2fd72c1
IamZombiez <dereksiu216@gmail.com>
2018-07-06 15:32:32 -0700

    reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,7 +1,14 @@
+import { CHANGE_SEARCH_FIELD } from '../constats.js';
+
 const initialState = {
   searchBox: ''
 }
 
 export const searchBots = (state=initialState, action={}) => {
-  
+  switch(action.type) {
+    case: CHANGE_SEARCH_FIELD:
+      return { ...state, searchBox:action.payload }
+    default:
+      return state;
+  }
 }

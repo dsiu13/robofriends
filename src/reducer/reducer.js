f7dbc41a443c55a1bcfb66a21870a57daa4f36de
IamZombiez <dereksiu216@gmail.com>
2018-07-11 13:09:31 -0700

    wrong search input name

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -12,7 +12,7 @@ const initialStateSearch = {
 export const searchBots = (state=initialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
-      return Object.assign({}, state, {searchField: action.payload})
+      return { ...state, searchBox:action.payload }
     default:
       return state;
   }

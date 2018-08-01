1ebfb226bb124c018263a873add80088739f9dad
IamZombiez <dereksiu216@gmail.com>
2018-08-01 13:53:32 -0700

    tests

--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -23,5 +23,46 @@ describe('searchRobots', () => {
       searchBox:'abc'
     })
   })
+})
+
+describe('requestRobots', () => {
+  initialStateRobots = {
+    robots: [],
+    isPending: true
+  }
+
+  it('expect to return initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual({searchBox: ''})
+  })
+
+  it('handle req robots pending action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_PENDING
+      payload: [{
+        id: '123',
+        name:'test',
+        email:'test@gmail.com'
+      }]
+    })).toEqual({
+      robots:[{
+        id: '123',
+        name:'test',
+        email:'test@gmail.com'
+      }],
+      isPending: true
+    })
+  })
+
+  it('handle req robots pending failed action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_FAILED,
+      payload: ':('
+    })).toEqual({
+      err:[{
+        ':('
+      }],
+      isPending: false
+    })
+  })
 
 })

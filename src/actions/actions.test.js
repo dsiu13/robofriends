4e48e0f5b92fdc3c84dc68c492f51544ae977b00
IamZombiez <dereksiu216@gmail.com>
2018-08-02 14:45:58 -0700

    action test

---
+++ src/actions/actions.test.js
@@ -0,0 +1,16 @@
+import * as actions from './actions';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from '../constants.js'
+
+it('should create an action to search robots', () => {
+  const text = "Hello";
+  const expectedAction = {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+  }
+  expect(actions.setSearchField(text)).toEqual(expectedAction);
+})
